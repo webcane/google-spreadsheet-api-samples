03aeb4a08fdbb21c49c017fd5fe63f867924287d
cane <webcane@ya.ru>
2017-02-05 20:34:33 +0300

    return inited

--- google-sheets-sample/src/main/java/cane/brothers/spring/service/GoogleSheetsService.java
+++ google-sheets-sample/src/main/java/cane/brothers/spring/service/GoogleSheetsService.java
@@ -16,40 +16,38 @@ public class GoogleSheetsService implements GoogleSheets {
 
 	@Value("${google.app.name}")
 	String appName;
-	
+
 	@Value("${google.spreadsheet.id}")
 	String spreadsheetId;
-	
+
 	@Value("${google.spreadsheet.sheet.name}")
 	String sheetName;
 
 	private Sheets sheetsService = null;
 
 	@Override
-	public List<List<Object>> readTable(GoogleConnection connection)  throws IOException {
+	public List<List<Object>> readTable(GoogleConnection connection) throws IOException {
 		Sheets service = getSheetsService(connection);
 		return readTable(service, spreadsheetId, sheetName);
 	}
-	
+
 	private Sheets getSheetsService(GoogleConnection gc) throws IOException {
 		if (this.sheetsService == null) {
-			return new Sheets.Builder(Global.HTTP_TRANSPORT, Global.JSON_FACTORY, gc.getCredentials())
+			this.sheetsService = new Sheets.Builder(Global.HTTP_TRANSPORT, Global.JSON_FACTORY, gc.getCredentials())
 					.setApplicationName(appName).build();
-		} else {
-			return this.sheetsService;
 		}
+		return this.sheetsService;
 	}
-	
-	private List<List<Object>> readTable(Sheets service, String spreadsheetId, String sheetName)  throws IOException {
-		ValueRange table = service.spreadsheets().values().get(spreadsheetId, sheetName)
-				.execute();
+
+	private List<List<Object>> readTable(Sheets service, String spreadsheetId, String sheetName) throws IOException {
+		ValueRange table = service.spreadsheets().values().get(spreadsheetId, sheetName).execute();
 
 		List<List<Object>> values = table.getValues();
 		printTable(values);
-		
+
 		return values;
 	}
-	
+
 	private void printTable(List<List<Object>> values) {
 		if (values == null || values.size() == 0) {
 			System.out.println("No data found.");
