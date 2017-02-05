3224947bcab06f1f9bec4cf16b920c669d5fffc3
cane <webcane@ya.ru>
2017-02-05 22:28:54 +0300

    add @EnableOAuth2Sso

--- google-sheets-sample/src/main/java/cane/brothers/spring/Application.java
+++ google-sheets-sample/src/main/java/cane/brothers/spring/Application.java
@@ -2,7 +2,9 @@ package cane.brothers.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
+@EnableOAuth2Sso
 @SpringBootApplication
 public class Application  {
 	
